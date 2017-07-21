0b2a3f456910eee720c8e7594186add7bcee5d5a
sreedeviss <sreedevi.suresh@antuit.com>
2017-07-21 12:04:34 +0530

    commit

--- src/main/scala/cipoc.scala
+++ src/main/scala/cipoc.scala
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 /***********editing from STS
-=======
+
 /************
->>>>>>> 44a30673d00511adb0a79df6859d268185f5af35
+
   * Created by hrishikesh.kshatriya on 7/12/2017.
   */
 import org.apache.spark.sql.DataFrame
@@ -18,25 +18,24 @@ import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.functions.lit
 import org.apache.spark.sql._
 import org.apache.spark.sql.hive._
-<<<<<<< HEAD
-=======
+
 
 import java.sql.DriverManager
 import java.sql.Connection
->>>>>>> 44a30673d00511adb0a79df6859d268185f5af35
+
 
 object cipoc {
  
 
 def main(args: Array[String]): Unit = {
 
-<<<<<<< HEAD
+
     val conf = new SparkConf().setAppName("CIPOC Test").setMaster("local[*]")
     val sc = new SparkContext(conf)
     val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
     import hiveContext.implicits._
     val sanSource = hiveContext.read.format("jdbc").
-=======
+
     val conf = new SparkConf().setAppName("CIPOC Test").setMaster("local[*]") 
     val sc = new SparkContext(conf)
     val hiveContext = new org.apache.spark.sql.hive.HiveContext(sc)
@@ -47,13 +46,13 @@ def main(args: Array[String]): Unit = {
     //val schema_rdd = StructType(schema_string.split(",").map(fieldName => StructField(fieldName, StringType, true)))
     //var sanSource = hiveContext.createDataFrame(sc.emptyRDD[Row], schema_rdd) 
                 val sanSource = hiveContext.read.format("jdbc").
->>>>>>> 44a30673d00511adb0a79df6859d268185f5af35
+
     option("url", "jdbc:postgresql://10.0.0.8:5432/postgrescommon").
     option("jdbc_driver_library" , "postgresql-9.4-1201-jdbc41.jar").
     option("driver", "org.postgresql.Driver").
     option("dbtable", "company").
     option("user", "postgrescommon").
-<<<<<<< HEAD
+
     option("password", "postgrescommon").load()
     sanSource.registerTempTable("sourceData")
     hiveContext.sql("INSERT INTO TABLE cipoc.sample SELECT * FROM sourceData")
@@ -62,7 +61,7 @@ def main(args: Array[String]): Unit = {
 
 
 }
-=======
+
     option("password", "postgrescommon").load() 
     sanSource.registerTempTable("sourceData")
     hiveContext.sql("truncate table cipoc.sample")
@@ -91,5 +90,3 @@ def main(args: Array[String]): Unit = {
   }
 }
 
-
->>>>>>> 44a30673d00511adb0a79df6859d268185f5af35
