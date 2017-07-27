0938722bcb659f2352181d28f207940cb1b7e2b8
sreedeviss <sreedevi.suresh@antuit.com>
2017-07-27 10:21:41 +0530

    Update CountTest.sh

--- CountTest.sh
+++ CountTest.sh
@@ -6,24 +6,29 @@ USERNAME=postgrescommon
 HOsTNAME=postgrescommon
 export PGPASSWORD=postgrescommon
 
+echo "before connecting to postgres"
 PCOUNT=`psql -U postgrescommon -p 5432 -h 52.229.20.59  << EOF
 select count(*) 
 from company
 EOF`
 
+echo "after getting count from postgres"
+
 PCOUNT1=${PCOUNT:21:2}
 PCOUNT2=$(echo $PCOUNT1 | tr -d ' ')
 #echo ${PCOUNT2}999
 
+echo "before connecting to hive"
 db_name=cipoc
 table_name=real_estate_trans
 HCOUNT1=`hive -S -e "select count(*) from cipoc.sample;"`
 HCOUNT2=$(echo $HCOUNT1 | tr -d ' ')
 #echo $HCOUNT2
+echo "after getting count from hive"
 
 #Comment below line for Successful Build, uncomment for Failure 
 #PCOUNT2=100 testtttttt
-
+echo "before comparing postgres and hive values"
 if [ "$PCOUNT2" == "$HCOUNT2" ]
 then
 	echo "[info] Integration -  Data Testing"
